e6cf9dfe8ad7245efd8e9ddc3512c25ef75111af
Clive <Clive.Oldridge2@dia.govt.nz>
2024-11-17 14:15:22 +1300

    Basic custom connector to remove special characters

---
+++ CustomConnectorTests/RemoveSpecialCharacters.cs
@@ -0,0 +1,57 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Net;
+
+namespace CustomConnectorTests
+{
+    public class Script : ScriptBase
+    {
+        public override async Task<HttpResponseMessage> ExecuteAsync()
+        {
+            if (this.Context.OperationId == "RemoveSpecialCharacters")
+            {
+                return await this.HandleRemoveSpecialCharactersOperation().ConfigureAwait(false);
+            }
+
+            HttpResponseMessage response = new HttpResponseMessage(HttpStatusCode.BadRequest);
+            response.Content = CreateJsonContent($"Unknown operation ID '{this.Context.OperationId}'");
+            return response;
+        }
+
+        private async Task<HttpResponseMessage> HandleRemoveSpecialCharactersOperation()
+        {
+            HttpResponseMessage response;
+
+            var contentAsString = await this.Context.Request.Content.ReadAsStringAsync().ConfigureAwait(false);
+
+            var contentAsJson = JObject.Parse(contentAsString);
+
+            var specialCharacters = contentAsJson["specialCharacters"].ToObject<string>();
+            var text = contentAsJson["text"].ToObject<string>();
+
+            var sanitizedText = RemoveSpecialCharacters(text, specialCharacters);
+
+            JObject output = new JObject
+            {
+                ["sanitizedText"] = sanitizedText,
+            };
+
+            response = new HttpResponseMessage(HttpStatusCode.OK);
+            response.Content = CreateJsonContent(output.ToString());
+            return response;
+        }
+
+        public static string RemoveSpecialCharacters(string targetString, string specialChars)
+        {
+            foreach (char specialChar in specialChars)
+            {
+                targetString = targetString.Replace(specialChar.ToString(), "");
+            }
+            return targetString;
+        }
+
+        private HttpContent CreateJsonContent(string json)
+        {
+            return new StringContent(json, System.Text.Encoding.UTF8, "application/json");
+        }
+    }
+}
